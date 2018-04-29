a7693c680529ee1854413dac4bba7c784cd3c003
macmillan kimutai <mcmyllano@gmail.com>
2018-04-29 18:58:56 +0300

    This is to commit that i have finished with the front end and backend of the project.

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -18,5 +18,8 @@ getProfileInfo(){
  return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
  .map(res => res.json());
   }
-
+ getProfileRepos(){
+   return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+   .map(res => res.json());
+ }
 }
